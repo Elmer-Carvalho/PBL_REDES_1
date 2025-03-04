70760b0c14536b56c1771e7738a3b1ebbeb84f2f
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2025-03-04 02:53:27 -0300

    First Commit

---
+++ server/models/eletric_vehicles.py
@@ -0,0 +1,14 @@
+class Electric_Vehicles:
+
+    def __init__(self, latitude, longitude, battery):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.battery = battery
+
+
+    def drive_to(self, destin_lat, destin_lon):
+        # Mecânica de Locomoção
+        pass
+    
+    def charge_at_station(self):
+        pass
